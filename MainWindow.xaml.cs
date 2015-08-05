26b86bfafc8943c4ff5c9cb749643dfccf0bb702
Rachel Rodini <Rachel.Rodini@amtrustgroup.com>
2015-08-05 09:56:44 -0400

    Initial commit

---
+++ MainWindow.xaml.cs
@@ -0,0 +1,49 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+using MahApps.Metro.Controls;
+using NeedHelpExamples.ViewModels;
+
+namespace NeedHelpExamples
+{
+    /// <summary>
+    /// Interaction logic for MainWindow.xaml
+    /// </summary>
+    public partial class MainWindow
+    {
+        private readonly MainViewModel _vm;
+        public MainWindow()
+        {
+            _vm = new MainViewModel();
+            DataContext = _vm;
+            InitializeComponent();
+        }
+
+        public void ShowFileFlyout(object sender, RoutedEventArgs e)
+        {
+            ToggleFlyout(0);
+        }
+
+        private void ToggleFlyout(int index)
+        {
+            var flyout = this.Flyouts.Items[index] as Flyout;
+            if (flyout == null)
+            {
+                return;
+            }
+
+            flyout.IsOpen = !flyout.IsOpen;
+        }
+    }
+}
